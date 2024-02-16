a1261317691216d5bf4195df9787afb3f53a7aa7
Solovitska <146887721+Solovitska@users.noreply.github.com>
2024-02-16 22:42:20 +0200

    up resume

---
+++ task.js
@@ -0,0 +1,8 @@
+
+    function showSkills(category) {
+      document.getElementById('programmingSkills').style.display = 'none';
+      document.getElementById('languagesSkills').style.display = 'none';
+   
+      document.getElementById(category + 'Skills').style.display = 'block';
+    }
+
